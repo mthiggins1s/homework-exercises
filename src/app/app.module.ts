2e872c352fb7cd9645de6974079f1e8969982d25
Matthew Higgins <mthiggins1s@semo.edu>
2025-03-24 20:15:48 -0500

    add card components to app module and integrate into app component

---
+++ src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { CardComponent } from './card/card.component';  // Import CardComponent
+
+@NgModule({
+
+  declarations: [],
+  imports: [
+    BrowserModule,
+    AppComponent,
+    CardComponent
+  ],
+  providers: [],
+})
+export class AppModule { }
